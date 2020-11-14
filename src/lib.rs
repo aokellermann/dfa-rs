acc4bdbb7d8db3d91170226463a2f607ec860756
Antony Kellermann <aokellermann@gmail.com>
2020-11-13 19:13:36 -0500

    Nfa to Dfa

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,8 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::{BTreeMap, BTreeSet, HashMap, HashSet};
+use std::iter::FromIterator;
 
 use serde::{Deserialize, Serialize};
 use serde_json;
-
-pub type State = String;
-pub type States = HashSet<State>;
 
 pub type Symbol = char;
 
@@ -13,8 +11,40 @@ pub const EPSILON: Symbol = 'ε';
 pub type Alphabet = HashSet<Symbol>;
 pub type Language = String;
 
+pub type State = String;
+pub type States = HashSet<State>;
 pub type Transitions = HashMap<Symbol, State>;
 pub type StateTransitions = HashMap<State, Transitions>;
+
+pub type NfaTransitions = HashMap<Symbol, States>;
+pub type NfaStateTransitions = HashMap<State, NfaTransitions>;
+
+type MultiState = BTreeSet<State>;
+type MultiStates = BTreeSet<MultiState>;
+type MultiNfaTransitions = HashMap<Symbol, MultiState>;
+type MultiNfaStateTransitions = BTreeMap<MultiState, MultiNfaTransitions>;
+
+trait ToState {
+    fn to_state(&self) -> State;
+}
+
+impl ToState for MultiState {
+    fn to_state(&self) -> State {
+        BTreeSet::from_iter(self.clone()).into_iter().collect::<Vec<String>>().join(",")
+    }
+}
+
+trait ToMultiState {
+    fn to_multi_state(&self) -> MultiState;
+}
+
+impl ToMultiState for State {
+    fn to_multi_state(&self) -> MultiState {
+        let mut mstate = MultiState::new();
+        mstate.insert(self.clone());
+        mstate
+    }
+}
 
 #[derive(Debug, PartialEq)]
 pub enum Acceptance {
@@ -33,9 +63,84 @@ pub struct Dfa {
     final_states: States,
 }
 
+#[derive(Serialize, Deserialize)]
+pub struct Nfa {
+    states: States,
+    alphabet: Alphabet,
+    state_transitions: NfaStateTransitions,
+    start_state: State,
+    final_states: States,
+}
+
+struct MultiNfa {
+    states: MultiStates,
+    alphabet: Alphabet,
+    state_transitions: MultiNfaStateTransitions,
+    start_state: MultiState,
+    final_states: MultiStates,
+}
+
 impl Dfa {
     pub fn new(json_string: &'static str) -> Dfa {
-        serde_json::from_str(json_string).unwrap()
+        let nfa: Nfa = serde_json::from_str(json_string).unwrap();
+        Dfa::from_nfa(nfa)
+    }
+
+    pub fn from_nfa(nfa: Nfa) -> Dfa {
+        match nfa.is_dfa() {
+            true => {
+                let mut transitions = StateTransitions::new();
+                for (state, nfa_transitions) in nfa.state_transitions.iter() {
+                    let state_transitions = transitions
+                        .entry(state.clone())
+                        .or_insert_with(||Transitions::new());
+                    for (symbol, states) in nfa_transitions {
+                        for state in states {
+                            state_transitions.insert(symbol.clone(), state.clone());
+                        }
+                    }
+                }
+
+                Dfa {
+                    states: nfa.states,
+                    alphabet: nfa.alphabet,
+                    state_transitions: transitions,
+                    start_state: nfa.start_state,
+                    final_states: nfa.final_states,
+                }
+            }
+            _ => {
+                let multi_nfa = MultiNfa::from_nfa(&nfa);
+                let states: States = multi_nfa.states
+                    .into_iter()
+                    .map(|state| state.to_state())
+                    .collect();
+                let alphabet: Alphabet = multi_nfa.alphabet;
+                let state_transitions: StateTransitions = multi_nfa.state_transitions
+                    .into_iter()
+                    .map(|(state, transitions)|
+                        (state.to_state(),
+                         transitions
+                             .into_iter()
+                             .map(|(symbol, to_state)| (symbol, to_state.to_state()))
+                             .collect()
+                        )
+                    )
+                    .collect();
+                let start_state: State = multi_nfa.start_state.to_state();
+                let final_states: States = multi_nfa.final_states
+                    .into_iter()
+                    .map(|state| state.to_state())
+                    .collect();
+                Dfa {
+                    states,
+                    alphabet,
+                    state_transitions,
+                    start_state,
+                    final_states,
+                }
+            }
+        }
     }
 
     pub fn get_states(&self) -> &States {
@@ -73,5 +178,130 @@ impl Dfa {
             Some(_) => Acceptance::Accepted,
             _ => Acceptance::Rejected,
         }
+    }
+}
+
+impl Nfa {
+    pub fn is_dfa(&self) -> bool {
+        !self.state_transitions.values()
+            .any(|state_transitions| state_transitions.iter()
+                .any(|(symbol, transitions)| symbol.eq(&EPSILON) || transitions.len() > 1))
+    }
+}
+
+impl MultiNfa {
+    pub fn from_nfa(nfa: &Nfa) -> MultiNfa {
+        let mut all_transitions = MultiNfaStateTransitions::new();
+
+        // First, find all states reachable by epsilon closure from the start state.
+        let mut epsilon_reachable_state = nfa.start_state.to_multi_state();
+        MultiNfa::epsilon_closure(&mut epsilon_reachable_state,
+                                  &nfa.start_state,
+                                  &nfa.state_transitions);
+
+        // Recursively find all states reachable by reading input from the start state.
+        MultiNfa::aggregate_transitions(&mut all_transitions,
+                                        &nfa.start_state.to_multi_state(),
+                                        &nfa.state_transitions);
+
+        let states: MultiStates = all_transitions.keys().cloned().collect();
+
+        let mut final_states = MultiStates::new();
+        for state in states.iter() {
+            for final_state in nfa.final_states.iter() {
+                if state.contains(final_state) {
+                    final_states.insert(state.clone());
+                }
+            }
+        }
+
+        MultiNfa {
+            states: states,
+            alphabet: nfa.alphabet.clone(),
+            state_transitions: all_transitions,
+            start_state: epsilon_reachable_state,
+            final_states: final_states,
+        }
+    }
+
+    fn epsilon_closure(reachable_mstate: &mut MultiState,
+                       initial_state: &State,
+                       state_transitions: &NfaStateTransitions) {
+        match state_transitions.get(initial_state) {
+            Some(initial_state_transitions) => match initial_state_transitions.get(&EPSILON) {
+                Some(initial_state_epsilon_transitions) =>
+                    for epsilon_transition_state in initial_state_epsilon_transitions.iter() {
+                        if reachable_mstate.insert(epsilon_transition_state.clone()) {
+                            // Recurse if new reachable state was inserted
+                            MultiNfa::epsilon_closure(reachable_mstate,
+                                                      epsilon_transition_state,
+                                                      state_transitions);
+                        }
+                    },
+                _ => ()
+            },
+            _ => ()
+        };
+    }
+
+    fn aggregate_transitions(all_transitions: &mut MultiNfaStateTransitions,
+                             current_state: &MultiState,
+                             state_transitions: &NfaStateTransitions) {
+        // Check if there is at least one transition for the current state.
+        if current_state.iter().any(|state| state_transitions.get(state).is_none()) {
+            return;
+        };
+
+        // Get epsilon closure before finding reachable states.
+        let mut reachable_current_mstate = current_state.clone();
+        for state in current_state {
+            MultiNfa::epsilon_closure(&mut reachable_current_mstate,
+                                      state,
+                                      state_transitions);
+        };
+
+        // Return if no transitions are possible
+        if all_transitions.get(&reachable_current_mstate).is_some() { return; };
+
+        // Get transitions for all reachable states.
+        let mut aggregated_current_state_transitions = MultiNfaTransitions::new();
+
+        for reachable_state in reachable_current_mstate.iter() {
+            match state_transitions.get(reachable_state) {
+                Some(reachable_state_transitions) => {
+                    for (symbol, reachable_state_transitions) in reachable_state_transitions {
+                        if symbol.ne(&EPSILON) {
+                            let mut reachable_states_with_epsilon_closure =
+                                reachable_state_transitions.clone().into_iter()
+                                    .collect::<MultiState>();
+                            for state in reachable_state_transitions {
+                                MultiNfa::epsilon_closure(
+                                    &mut reachable_states_with_epsilon_closure,
+                                    state,
+                                    state_transitions);
+                            }
+
+                            aggregated_current_state_transitions
+                                .entry(symbol.clone())
+                                .or_insert_with(|| MultiState::new())
+                                .extend(reachable_states_with_epsilon_closure);
+                        }
+                    }
+                }
+                _ => ()
+            }
+        };
+
+        // Add transitions for current state to all_transitions so they aren't duplicated when we recurse.
+        all_transitions.insert(reachable_current_mstate, aggregated_current_state_transitions
+            .clone());
+
+        let reachable_states =
+            aggregated_current_state_transitions.values().cloned().collect::<MultiStates>();
+
+        // Recurse on reachable states
+        for reachable_state in reachable_states.iter() {
+            MultiNfa::aggregate_transitions(all_transitions, reachable_state, state_transitions);
+        };
     }
 }
--- tests/dfa-test.rs
+++ tests/dfa-test.rs
@@ -20,19 +20,51 @@ const TEST_DFA_JSON1: &'static str = r#"{
           "final_states": ["q2"],
           "state_transitions": {
             "q1": {
-              "0": "q1",
-              "1": "q2"
+              "0": ["q1"],
+              "1": ["q2"]
             },
             "q2": {
-              "0": "q3",
-              "1": "q2"
+              "0": ["q3"],
+              "1": ["q2"]
             },
             "q3": {
-              "0": "q2",
-              "1": "q2"
+              "0": ["q2"],
+              "1": ["q2"]
             }
           }
         }"#;
+
+const TEST_NFA_JSON1: &'static str = r#"{
+	"states": [
+		"q0",
+		"q1",
+		"q2",
+		"q3"
+	],
+	"alphabet": [
+		"a",
+		"b"
+	],
+	"start_state": "q0",
+	"final_states": ["q0"],
+	"state_transitions": {
+		"q0": {
+			"ε": ["q1"]
+		},
+		"q1": {
+			"a": ["q1", "q2"],
+			"b": ["q2"]
+		},
+		"q2": {
+			"a": ["q0", "q2"],
+			"b": ["q3"]
+		},
+		"q3": {
+			"b": ["q1"]
+		}
+	}
+}
+"#;
 
 #[test]
 fn deserialize() {
@@ -125,4 +157,116 @@ fn invalid_alphabet() {
     assert_eq!(dfa.accepts("111c00".parse().unwrap()), Acceptance::InvalidAlphabet);
     assert_eq!(dfa.accepts("111020".parse().unwrap()), Acceptance::InvalidAlphabet);
     assert_eq!(dfa.accepts("1-11c00".parse().unwrap()), Acceptance::InvalidAlphabet);
+}
+
+#[test]
+fn deserialize_nfa() {
+    let dfa = Dfa::new(TEST_NFA_JSON1);
+
+    let states = dfa.get_states();
+    assert!(states.contains("q1"));
+    assert!(states.contains("q2"));
+    assert!(states.contains("q3"));
+    assert!(states.contains("q0,q1"));
+    assert!(states.contains("q1,q2"));
+    assert!(states.contains("q1,q3"));
+    assert!(states.contains("q2,q3"));
+    assert!(states.contains("q0,q1,q2"));
+
+    let alphabet = dfa.get_alphabet();
+    assert!(alphabet.contains(&'a'));
+    assert!(alphabet.contains(&'b'));
+
+    assert_eq!(dfa.get_start_state(), "q0,q1");
+    assert!(dfa.get_final_states().contains("q0,q1"));
+    assert!(dfa.get_final_states().contains("q0,q1,q2"));
+
+    let state_transitions = dfa.get_state_transitions();
+    let expected_transitions = [
+        DfaTransition {
+            s1: "q1".parse().unwrap(),
+            symbol: "a".parse().unwrap(),
+            s2: "q1,q2".parse().unwrap(),
+        },
+        DfaTransition {
+            s1: "q1".parse().unwrap(),
+            symbol: "b".parse().unwrap(),
+            s2: "q2".parse().unwrap(),
+        },
+        DfaTransition {
+            s1: "q2".parse().unwrap(),
+            symbol: "a".parse().unwrap(),
+            s2: "q0,q1,q2".parse().unwrap(),
+        },
+        DfaTransition {
+            s1: "q2".parse().unwrap(),
+            symbol: "b".parse().unwrap(),
+            s2: "q3".parse().unwrap(),
+        },
+        DfaTransition {
+            s1: "q3".parse().unwrap(),
+            symbol: "b".parse().unwrap(),
+            s2: "q1".parse().unwrap(),
+        },
+        DfaTransition {
+            s1: "q0,q1".parse().unwrap(),
+            symbol: "a".parse().unwrap(),
+            s2: "q1,q2".parse().unwrap(),
+        },
+        DfaTransition {
+            s1: "q0,q1".parse().unwrap(),
+            symbol: "b".parse().unwrap(),
+            s2: "q2".parse().unwrap(),
+        },
+        DfaTransition {
+            s1: "q1,q2".parse().unwrap(),
+            symbol: "a".parse().unwrap(),
+            s2: "q0,q1,q2".parse().unwrap(),
+        },
+        DfaTransition {
+            s1: "q1,q2".parse().unwrap(),
+            symbol: "b".parse().unwrap(),
+            s2: "q2,q3".parse().unwrap(),
+        },
+        DfaTransition {
+            s1: "q1,q3".parse().unwrap(),
+            symbol: "a".parse().unwrap(),
+            s2: "q1,q2".parse().unwrap(),
+        },
+        DfaTransition {
+            s1: "q1,q3".parse().unwrap(),
+            symbol: "b".parse().unwrap(),
+            s2: "q1,q2".parse().unwrap(),
+        },
+        DfaTransition {
+            s1: "q2,q3".parse().unwrap(),
+            symbol: "a".parse().unwrap(),
+            s2: "q0,q1,q2".parse().unwrap(),
+        },
+        DfaTransition {
+            s1: "q2,q3".parse().unwrap(),
+            symbol: "b".parse().unwrap(),
+            s2: "q1,q3".parse().unwrap(),
+        },
+        DfaTransition {
+            s1: "q0,q1,q2".parse().unwrap(),
+            symbol: "a".parse().unwrap(),
+            s2: "q0,q1,q2".parse().unwrap(),
+        },
+        DfaTransition {
+            s1: "q0,q1,q2".parse().unwrap(),
+            symbol: "b".parse().unwrap(),
+            s2: "q2,q3".parse().unwrap(),
+        },
+    ];
+
+    for expected_transition in expected_transitions.iter() {
+        let transitions = state_transitions
+            .get(&expected_transition.s1)
+            .expect("Transitions not found");
+        let transition = transitions
+            .get(&expected_transition.symbol)
+            .expect("Transition not found");
+        assert_eq!(&expected_transition.s2, transition, "Incorrect transition");
+    }
 }
